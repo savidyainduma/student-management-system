ca462b6e933a148d4104a6e959cc9a8d155e6a69
savidyainduma <Savidyainduma@gmail.com>
2024-10-22 09:22:49 +0530

    created put method

--- src/main/java/com/example/backend/controller/StudentController.java
+++ src/main/java/com/example/backend/controller/StudentController.java
@@ -1,9 +1,11 @@
 package com.example.backend.controller;
 
 
+import com.example.backend.exception.ResourceNotFoundException;
 import com.example.backend.model.Student;
 import com.example.backend.repository.StudentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,5 +25,21 @@ public class StudentController {
     @PostMapping
     public Student addStudent(@RequestBody Student student) {
         return studentRepository.save(student);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable int id, @RequestBody Student studentDetails) {
+        Student updatedStudent = studentRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Student not found for this id :: " + id));
+
+        updatedStudent.setFullName(studentDetails.getFullName());
+        updatedStudent.setBirthDate(studentDetails.getBirthDate());
+        updatedStudent.setGender(studentDetails.getGender());
+        updatedStudent.setContactNumber(studentDetails.getContactNumber());
+        updatedStudent.setAddress(studentDetails.getAddress());
+        updatedStudent.setParentContact(studentDetails.getParentContact());
+
+        studentRepository.save(updatedStudent);
+        return ResponseEntity.ok(updatedStudent);
     }
 }
