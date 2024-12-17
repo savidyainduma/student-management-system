1caf0a17a92d09be347a65c7300ee4af3322d919
savidyainduma <Savidyainduma@gmail.com>
2024-12-17 16:33:55 +0530

    started working on logout

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './Header.css'
-
+import { HiOutlineUserCircle } from "react-icons/hi2";
 const Header = () => {
   return (
     <div className='header'>
+        <div className="header-title">
         <h1>PupilPro</h1>
+        </div>
+        <HiOutlineUserCircle color='white' className='user-icon' style={{fontSize:"45px", marginRight:"20px", cursor:"pointer"}} onClick={} />
+        
+        
     </div>
   )
 }
